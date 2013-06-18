cfa8e687c27e74833f5052faec417c17051186d1
Camara Lerner <camara.lerner@gmail.com>
2013-06-18 16:56:02 -0600

    finished 11517

--- 11284/11284.cc
+++ 11284/11284.cc
@@ -1,15 +1,16 @@
 #include <iostream>
 #include <cassert>
 #include <limits.h>
+#include <iomanip>
 
 using namespace std;
 
-const int MAX_NODES = 50;
-
+const int MAX_NODES = 51;
+const int OPERAS = 13;
 // subset and end point
-int data[4096][12];
+int data[4096][OPERAS];
 int dist[MAX_NODES][MAX_NODES];
-pair<int, int> opera[12];
+pair<int, int> opera[OPERAS];
 
 void floyd(const int& n)
 {
@@ -33,32 +34,45 @@ void floyd(const int& n)
    }
 }
 
-int recur(int set, int v, const int& n) {
+int recur(int set, int end, const int& p) {
    
-   if(data[set][v] != INT_MIN) {
-      return data[set][v];
-   } else if() { // 
-
+   if(data[set][end] != INT_MIN) {
+        return data[set][end];
+   } else if(set == (1 << end)) { // you are at the end
+      return data[set][end] = opera[end].second
+	 - dist[0][opera[end].first];      
+   } else {
+      int high = INT_MIN;
+      for(int v = 0; v < p; v++) {
+	 int j = (1 << v);
+	 int n_set = set - (1 << end);
+	 if((n_set & j) && dist[opera[v].first][opera[end].first] != INT_MIN) {
+	    data[set][end] = opera[end].second
+	       - dist[opera[v].first][opera[end].first] + recur(n_set, v, p);
+	    if(data[set][end] > high) {
+	       high = data[set][end];
+	    }
+	 }
+      }
+      return high;
    }
-   
-
-   
 }
 
 int subsets(const int& n, const int& m, const int& p) {
 
-   for(int i = 0; i < 4096; ++i){
-      fill(data[i], data[i] + 12, INT_MIN);
+   for(int i = 0; i < 4096; ++i) {
+      fill(data[i], data[i] + OPERAS, INT_MIN);
    }
    int max = INT_MIN;
-   for(int i = 0; i < (1 << p); ++i) {
-      for(int v = 0; v < 12; ++v) {
-	 int j = 1 << v;
-	 if(j ( i)) {
-	    int temp = recur(i, v, n);
+   for(int i = 1; i < (1 << p); ++i) {
+      for(int v = 0; v < OPERAS; ++v) {
+	 int j = (1 << v);
+	 if((j & i) && opera[v].first != INT_MIN) {
+	    int temp = recur(i, v, p) - dist[opera[v].first][0];
 	    if(temp > max) {
 	       max = temp;
 	    }
+	    //cerr << "i: " << i << " j: " << j << " cost: " << temp << endl;
 	 }
       }
    }
@@ -74,8 +88,8 @@ int main(void)
       int n, m;     
       cin >> n >> m;
       /* clear graph */
-      for (int i = 0; i < n; i++) {
-	 for (int j = 0; j < n; j++) {
+      for (int i = 0; i <= n; i++) {
+	 for (int j = 0; j <= n; j++) {
 	    dist[i][j] = INT_MIN;
 	 }
       }
@@ -88,20 +102,33 @@ int main(void)
 	 double w;
 	 cin >> i >> j >> w;
 //	 assert(0 <= i && i < n && 0 <= j && j < n);
-	 dist[i][j] = dist[j][i] = (int)(w*100 + 0.5);
+	 if((dist[i][j] == INT_MIN) || (dist[i][j] > (int)(w*100 + 0.5))) {
+	    dist[i][j] = dist[j][i] = (int)(w*100 + 0.5);
+	 }
       }
       
       
-      floyd(n);
+      floyd(n + 1);
 
       int p;
+      cin >> p;
+      fill(opera, opera + p, make_pair(INT_MIN,INT_MIN));
       double price;
+      int place;
       for(int i = 0; i < p; ++i) {
-	 cin >> opera[i].first >> price;
-	 opera[i].second = (int)(price*100 + 0.5);
+	 cin >> place >> price;
+	 opera[i] = make_pair(place, (int)(price*100 + 0.5));
       }
-      
-      int cents = subsets(n, m, p);
+
+      // finds the route that save the most money
+      int cents = subsets(n + 1, m, p);
+
+      if(cents <= 0) {
+	 cout << "Don't leave the house" << endl;
+      } else {
+	 cout << "Daniel can save $" << cents/100 << "." << setfill('0')
+	      << setw(2) << cents%100 << endl;
+      }
       
    }
 
